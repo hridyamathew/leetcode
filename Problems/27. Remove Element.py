1cad9c9ed6c14b1971afe777005eeada960192bc
Hridya Mathew <95233518+hridyamathew@users.noreply.github.com>
2026-02-09 11:15:20 +0530

    Create 27. Remove Element.py

---
+++ Problems/27. Remove Element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k = 0
+
+        for n in nums:
+            if n!=val:
+                nums[k] = n
+                k += 1
+        
+        return k
